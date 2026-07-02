a00de7ec52004acafda291110aa28ea5e908989a
Yyw-7 <771305429@qq.com>
2026-07-02 11:05:08 +0800

    fix: 中国地图 GeoJSON 本地化

--- src/components/Travel/EChartsMap.vue
+++ src/components/Travel/EChartsMap.vue
@@ -75,7 +75,7 @@ const provinceFullNameMap = {
 const loadChinaMap = async () => {
   try {
     const response = await fetch(
-      'https://geo.datav.aliyun.com/areas_v3/bound/100000_full.json'
+      `${import.meta.env.BASE_URL}china.json`
     )
     const geoJson = await response.json()
     echarts.registerMap('china', geoJson)
